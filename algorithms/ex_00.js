902ff7d7bfaf5679d9e9cb53996e19a924e30748
Sondro <memoiran@hotmail.com>
2018-02-19 06:39:57 -0800

    commit

--- algorithms/ex_00.js
+++ algorithms/ex_00.js
@@ -4,34 +4,35 @@ Analysis of Algorithms
 Exercise I. Give an runtime analysis for the input size n of each:
 
 **********************************************/
-// a)  O(n1)
-a = 0;
+// a):  O(n1)
+let a = 0;
 while (a < n * n * n) {
   a = a + n * n;
 }
 
-// b) // input array is of length n
-i = array.length - 1;
+// b): 
+// input array is of length n
+let i = array.length - 1;
 while (array[i] > x && i >= 0) {
   i = i / 2;
 }
 
-// c) 
-sum = 0;
+// c):
+let sum = 0;
 for (i = 0; i < Math.sqrt(n) / 2; i++) {
   for ( j = i; j < 8 + i; j++) {
     for (k = j; k < 8 + j; k++) { sum++; }
   }
 }
 
-// d) 
-sum = 0;
+// d): 
+let sum = 0;
 for (i = 1; i < n; i *= 2) {
   for (j = 0; j < n; j++) { sum++; }
 }
 
-// e) 
-sum = 0;
+// e): 
+let sum = 0;
 for (i = 0; i < n; i++) {
   for (j = i + 1; j < n; j++) {
     for (k = j + 1; k < n; k++) {
@@ -40,13 +41,13 @@ for (i = 0; i < n; i++) {
   }
 }
 
-// f) 
+// f:) 
 bunnyEars = function (bunnies) { // here bunnies === n
   if (bunnies === 0) { return 0; }
   return 2 + bunnyEars(bunnies - 1);
 }
 
-// g) 
+// g): 
 search = function (array, arraySize, target) { // here arraySize === n
   if (arraySize > 0) {
     if (array[arraySize-1] === target) { return true; }
@@ -60,13 +61,32 @@ search = function (array, arraySize, target) { // here arraySize === n
 // Exercise II:
 
 // ================================================================
-/*
+
+/*********************************************************************************************************
 a) Given an array a of n numbers, design a linear running time algorithm to find the maximum value of
   a[j] - a[i], where j ≥ i.
+**********************************************************************************************************/ 
+if (j >= i) { 
+  a[j] - a[i];
+}
+/*********************************************************************************************************
 b) Suppose that you have an n-story building and plenty of eggs. Suppose also that an egg is broken if it
     is thrown off floor f or higher, and unbroken otherwise. Devise a strategy to determine the value of f
     such that the number of dropped eggs is minimized.
-*/
+**********************************************************************************************************/ 
+
+function testEggs(eggs) {
+  const len = eggs.length;
+  let f = 0;
+  for (;f < len ; f++) {
+    if(isBroke(eggs[i], f)) { return f;} 
+}
+
+function setupTest() {
+  const eggs = [false, false, false, false, false];
+  const breaksAt = testEggs(eggs);
+}
+
 // ================================================================
 
 // Exercise III:
@@ -85,10 +105,10 @@ function quicksort(array) {
   
   for (x in array) {
   if (x <= pivot) {
-     //concat(x, less);
-     less.push(x);
+     concat(x, less);
+    
   }
-  else { greater.push(x); } //concat(x, greater);
+  else { concat(x, greater); }
   return concat(quicksort(less), list(pivot), quicksort(greater));
 }
 /*
